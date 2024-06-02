11d89495608547f02319e8b9b1ac1e93b371852a
lex0c <lex0c@proton.me>
2024-06-02 13:27:14 -0300

    fix openssl include

--- etc.h
+++ etc.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <openssl/evp.h>
+
 void xor(char *x_str);
 void cleanup(void *var, int len) __attribute__((visibility("hidden")));
 int extract_pid(const char *path) __attribute__((visibility("hidden")));
